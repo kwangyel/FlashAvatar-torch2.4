bba1e05cca9f913013988592f7bc94d9b267e30b
kinley wangyel <kwangyel@gmail.com>
2026-06-08 15:06:28 +0600

    sdf

--- scripts/setup_runpod_env.sh
+++ scripts/setup_runpod_env.sh
@@ -8,26 +8,32 @@ pip install ninja
 # User-requested PyTorch3D installation path.
 python -m pip install --no-build-isolation "git+https://github.com/facebookresearch/pytorch3d.git@stable"
 
+cd FlashAvatar-torch2.4
 # Project Python dependencies.
 python -m pip install --no-build-isolation -r requirements-runpod.txt
+
 pip install gdown
-gdown https://drive.google.com/file/d/1LRf0fdlGDq_gTiJysaCmtub6EUodC6Y0/view?usp=sharing -O kinley.zip
 gdown https://drive.google.com/file/d/1455XCYSZWmfILCYKS8IPBwKJaIF-zsri/view?usp=sharing -O FLAME2020.zip
 gdown https://drive.google.com/file/d/1c9GPL7K7vgVDEHhOxXZrr08P4KP4Ef4R/view?usp=sharing -O FLAME_masks.zip
+gdown https://drive.google.com/file/d/1QtBlUS0OLqyenEh-8uhngM2eaoppqsGK/view?usp=sharing -O masks.tar.gz
+gdown https://drive.google.com/file/d/1T8OK_zuhv0BbJLC6sHmmQPOSQ7lTgGC8/view?usp=sharing -O dataset.tar.gz
+gdown https://drive.google.com/file/d/1rDHSHglFoBBTh9vqjEq5SGH3HkN3rVwq/view?usp=sharing -O metrical-tracker.zip
 
 apt-get update
 apt-get install unzip
 
-mkdir -p kinley
 mkdir -p FLAME2020
 mkdir -p FLAME_masks
+mkdir -p masks
+mkdir -p dataset
+mkdir -p metrical-tracker
 
-unzip -d kinley.zip -q ./kinley
 unzip -q FLAME2020.zip -d ./FLAME2020
 unzip -q FLAME_masks.zip -d ./FLAME_masks
-
-mv kinley/kinley/dataset dataset
-mv kinley/kinley/metrical-tracker metrical-tracker
+tar -xzf masks.tar.gz -C ./masks
+tar -xzf dataset.tar.gz -C ./dataset
+unzip -q metrical-tracker.zip -d ./metrical-tracker
+mkdir -p flame/FLAME_masks
 
 mv FLAME2020/FLAME2020/generic_model.pkl flame/generic_model.pkl
 mv FLAME_masks/FLAME_masks.pkl flame/FLAME_masks/FLAME_masks.pkl
@@ -41,7 +47,7 @@ pip install yacs
 python scripts/verify_cuda_extensions.py
 
 # for renaminig the dataset since i messed up on the name of the dataset
-cd ~/FlashAvatar-torch2.4/dataset/kinley/parsing
+cd ~/FlashAvatar-torch2.4/dataset/Obama/parsing
 # actually rename
 for f in *_headneck.png; do mv "$f" "${f/_headneck/_neckhead}"; done
 
